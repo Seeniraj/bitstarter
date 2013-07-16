37914d714d97bc7e673d18fa8b389024dd2bc67f
seeniraj <seeniraj@hotmail.com>
2013-07-16 04:05:53 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = request('fs');
+var content = fs.readFileSync('index.html');
+var strContent = content.toString();
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(strContent);
 });
 
 var port = process.env.PORT || 5000;
